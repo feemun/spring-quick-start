944c60a188b3888673ddd1224dbd4b54c09d0e81
feemun <31443333+feemun@users.noreply.github.com>
2025-11-24 14:12:57 +0800

    全局异常处理

--- common/src/main/java/cloud/catfish/common/exception/GlobalExceptionHandler.java
+++ common/src/main/java/cloud/catfish/common/exception/GlobalExceptionHandler.java
@@ -70,7 +70,7 @@ public class GlobalExceptionHandler {
         if (StrUtil.isNotEmpty(message) && message.contains("denied")) {
             message = "演示环境暂无修改权限，如需修改数据可本地搭建后台服务！";
         }
-        return new ResponseEntity<>(R.failed("5/0"), HttpStatusCode.valueOf(500));
+        return new ResponseEntity<>(R.failed(message), HttpStatusCode.valueOf(500));
     }
 
     @ExceptionHandler(value = Exception.class)
@@ -79,7 +79,7 @@ public class GlobalExceptionHandler {
         if (StrUtil.isNotEmpty(message) && message.contains("denied")) {
             message = "演示环境暂无修改权限，如需修改数据可本地搭建后台服务！";
         }
-        return new ResponseEntity<>(R.failed("5/0"), HttpStatusCode.valueOf(500));
+        return new ResponseEntity<>(R.failed(message), HttpStatusCode.valueOf(500));
     }
 
     @MessageExceptionHandler
