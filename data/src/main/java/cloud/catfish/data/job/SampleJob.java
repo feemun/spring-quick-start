0e3d2b1c61f9128331fd96b902e7bd4bf5916125
feemun <feemun@outlook.com>
2026-01-05 11:45:01 +0800

    feat: 集成xxljob

---
+++ data/src/main/java/cloud/catfish/data/job/SampleJob.java
@@ -0,0 +1,31 @@
+package cloud.catfish.data.job;
+
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * XXL-JOB 示例任务
+ */
+@Component
+public class SampleJob {
+    private static Logger logger = LoggerFactory.getLogger(SampleJob.class);
+
+    /**
+     * 1、简单任务示例（Bean模式）
+     */
+    @XxlJob("demoJobHandler")
+    public void demoJobHandler() throws Exception {
+        XxlJobHelper.log("XXL-JOB, Hello World.");
+        
+        for (int i = 0; i < 5; i++) {
+            XxlJobHelper.log("beat at:" + i);
+            TimeUnit.SECONDS.sleep(2);
+        }
+        // default success
+    }
+}
